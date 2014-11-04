89f322fadced168f31633fa00da49dc3ca338a25
bloomlj <kolocc@live.com>
2014-11-05 00:45:58 +0800

    test use curl

--- routes/devices.js
+++ routes/devices.js
@@ -3,25 +3,86 @@ var router = express.Router();
 
 /* GET devices listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  var  knex = req.app.get('knex');
+  knex.select('*').from('devices')
+  .then(function(rows) {
+   console.log(rows);
+   res.render('devices/index', { title: 'devices list' });
+  });
+    
+  //res.send('respond with a resource');
+});
+
+/* create a device form. */
+router.get('/createform', function(req, res) {
+
+   res.render('devices/createform', { title: 'Express' });
+    
 });
 
 /* read a device. */
 router.get('/:id', function(req, res) {
-    console.log(req.params.id);
-  res.send('respond with a resource'+req.params.id);
+  
+  var  knex = req.app.get('knex');
+  knex('devices').where('id', req.params.id)
+  .then(function(rows) {
+   console.log(rows);
+   res.render('devices/detail', { title: 'Express' });
+  });
+    
+    
+    //console.log(req.params.id);
+  //res.send('respond with a resource'+req.params.id);
+});
+
+/* read a device. */
+router.get('/:id/editform', function(req, res) {
+  
+  var  knex = req.app.get('knex');
+  knex('devices').where('id', req.params.id)
+  .then(function(rows) {
+   console.log(rows);
+   res.render('devices/editform', { title: 'Express' });
+  });
+    
+    //console.log(req.params.id);
+  //res.send('respond with a resource'+req.params.id);
 });
 
 /* update a device. */
 router.put('/:id', function(req, res) {
+    var  knex = req.app.get('knex');
+   knex('sensors')
+  .where('name', '=', 'pir')
+  .update({
+    note: 'updated'
+  })
+    .then(function(ret){
+    console.log(ret)
+    });
+    
     console.log(req.params.id);
   res.send('respond with a resource'+req.params.id);
 });
 
+
+
 /* create a device. */
+//test:curl --data "name=curltestxx&category=1" http://localhost:3000/devices
+
 router.post('/', function(req, res) {
-    console.log(req);
-  res.send('respond with a resource'+req.params.id);
+    console.log(req.param('name'));
+    console.log(req.body);
+    var  knex = req.app.get('knex');
+      knex('devices')
+     .insert(req.body)
+ // .insert([{name: 'pir'},{name: 'camera'}])
+    .then(function(ret){
+    console.log(ret);
+        res.send('respond with a resource'+ret);
+    });
+    //console.log(req);
+  //res.send('respond with a resource'+req.params.id);
 });
 
 /* delete a device. */
